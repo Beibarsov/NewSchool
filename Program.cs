68c68e4ab4743f59877c8829a331999ae8287e80
Beibarsov <BarsStudio@gmail.com>
2022-01-28 19:44:27 +0800

    11

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Newschool{
+
+
+class MainClass {
+  public static void Main (string[] args) {
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Semaphore" + Semaphore.Green);
+  }
+
+  
+
+  
+}
+
+enum Semaphore{
+      Red = 100,
+      Yellow = 200,
+      Green = 300
+  }
+}
